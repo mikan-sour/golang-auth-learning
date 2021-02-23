495adedaab2fab643c4191e0a7eb345f8b0435c6
Jed Zeins <jedzeins@gmail.com>
2021-02-23 23:12:01 +0900

    encoding endpoint

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -48,6 +50,16 @@ func main() {
 }
 func foo(w http.ResponseWriter, r *http.Request) {
 
+	p1 := person{
+		First: "Jed",
+		Last:  "Zeins",
+	}
+
+	err := json.NewEncoder(w).Encode(p1)
+
+	if err != nil {
+		log.Println("encoding issue", err)
+	}
 }
 
 func bar(w http.ResponseWriter, r *http.Request) {
